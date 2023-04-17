21a83d99ebd758521073665ee1c714d6e37ba849
GiuseppeCerto <116110501+GiuseppeCerto@users.noreply.github.com>
2023-04-17 16:41:18 +0200

    EDIT create, edit and update crud direction

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -6,18 +6,42 @@
     <div class="main_jumbo"></div>
     <div class="bg_color">
         <div class="main_container">
-            @foreach ($comics as $comic)
-                <div class="container">
-                    <div class="row">
-                        <div class='card_comic col-4'>
-                            <a href="{{ route ('comics.show',[ 'comic' => $comic->id ]) }}">
-                                <img src="{{ $comic ['thumb']}}" alt="">
-                                <div class="card_title">{{ $comic ['series'] }}</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Image</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Descriprion</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($comics as $comic)
+                    <tr>
+                    <td>
+                        <img src="{{ $comic->thumb }}" alt="" height="140">
+                    </td>
+                    <th scope="row">
+                        {{ $comic->price }}
+                    </th>
+                    <td>
+                        <a href="{{ route('comics.show',$comic->id) }}">
+                        {{ $comic->title }}
+                        </a>
+                    </td>
+
+                    <th scope="row">
+                        {{ $comic->description }}
+                    </th>
+                    
+                    <td>
+                        <a class="btn btn-secondary btn-sm" href="{{ route('comics.edit',$comic) }}">Edit</a>
+                    </td>
+                    </tr>
+                        
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 </section>
